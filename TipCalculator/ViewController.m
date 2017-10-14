7857df965bfb6fb1ca77e79086a373d91d6a3edd
Andrew Shopland <hi@ashp.land>
2017-10-13 17:54:23 -0700

    Implement text input and tip/total update

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *percentAmountTextField;
 @property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalAmountLabel;
+@property (assign, nonatomic) float currentSubtotal;
+@property (assign, nonatomic) float tipPercentage;
 
 @end
 
@@ -20,7 +22,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.tipPercentage = 0.15;
     
 }
 
@@ -32,18 +34,50 @@
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
 {
-    NSLog(@"%@", string);
-    NSLog(@"%@", textField.text);
     
-    //textField.text = @"$";
+    // TODO: implement delete
     
-    int newSubtotal = [@"9.25" floatValue];
+    bool newStringIsntANumber = [string rangeOfCharacterFromSet:[NSCharacterSet decimalDigitCharacterSet]].location != 0;
     
-    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%.2f", newSubtotal * 0.15];
-
-    return YES;
+    if (newStringIsntANumber) {
+        return NO;
+    }
+    
+//    NSString *oldValue;
+//
+//    if (textField.text.length == 0) {
+//        oldValue = @"$0.00";
+//    } else {
+//        oldValue = textField.text;
+//    }
+    
+    //NSString *oldValue = (textField.text.length == 0) ? @"$0.00" : textField.text;
+    
+    self.currentSubtotal = [self getFloatFromString:textField.text andAdd:string];
+    
+    textField.text = [NSString stringWithFormat:@"$%.2f", self.currentSubtotal];
+    
+    [self updateTipAndTotal];
+    
+    return NO;
 }
 
+-(float)getFloatFromString:(NSString *)currentString andAdd:(NSString *)nextCharacter
+{
+    NSString *cleanString = [[[currentString stringByReplacingOccurrencesOfString:@"$" withString:@""]
+                                              stringByReplacingOccurrencesOfString:@"." withString:@""]
+                                                 stringByAppendingString:nextCharacter];
+    
+    return [cleanString floatValue] * 0.01;
+}
+
+-(void)updateTipAndTotal
+{
+    float tipAmount = self.currentSubtotal * self.tipPercentage;
+    
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%.2f", tipAmount];
+    self.totalAmountLabel.text = [NSString stringWithFormat:@"$%.2f", self.currentSubtotal + tipAmount];
+}
 
 
 @end
